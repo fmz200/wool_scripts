202ea6af2e94533ae035c689f83a02cca9904b8d
郑一笙 <zfm6633@gmail.com>
2023-04-22 09:27:32 +0800

    更新重写：App&小程序净化合集

--- scripts/server_rename_dev.js
+++ scripts/server_rename_dev.js
@@ -1,10 +1,10 @@
 //############################################
 // 原始地址：https://github.com/sub-store-org/Sub-Store/blob/master/scripts/ip-flag.js
-// 脚本地址：https://raw.githubusercontent.com/fmz200/wool_scripts/main/scripts/server_rename.js
+// 脚本地址：https://raw.githubusercontent.com/fmz200/wool_scripts/main/scripts/rename_simple.js
 // 脚本作用：在SubStore内对节点重命名为：旗帜|地区代码|地区名称|IP|序号，
 // 使用方法：SubStore内选择“脚本操作”，然后填写上面的脚本地址
 // 支持平台：目前只支持Loon，Surge
-// 更新时间：2023.04.18 22:20
+// 更新时间：2023.04.21 22:20
 // 这个脚本是测试脚本，请使用 server_rename.js
 //############################################
 
