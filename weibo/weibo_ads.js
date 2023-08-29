c8c3670a7c4aeb58657e31cc1ac914fdbf55bbbe
king <zfm6633@gmail.com>
2023-08-29 21:32:25 +0800

    Update and rewrite: Weibo removes advertisements

--- weibo/weibo_ads.js
+++ weibo/weibo_ads.js
@@ -16,35 +16,54 @@ const url4 = '/api.weibo.cn/2/page'; // 微博热搜页面url
 const url5 = '/statuses/container_timeline_topicpage'; // 微博超话页面
 const url6 = '/statuses/extend'; // 微博详情页面广告
 
+const weiboPic = [
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-01.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-19.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-20.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-27.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-30.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-32.png",
+  "https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/Weibo-43.png"
+]
+
 let index = 1;
 let url = $request.url;
 let body = $response.body;
-body = modifyMain(url, body);
+try {
+  body = modifyMain(url, body);
+} catch (e) {
+  console.log('脚本运行出现错误，返回源数据⚠️');
+  console.log('错误信息：' + e.message);
+}
 $done({body});
 
 function modifyMain(url, data) {
   let resp_data = JSON.parse(data);
   // 1、首次点击发现按钮
   if (url.includes(url1)) {
-    const channel = resp_data.channelInfo?.channels?.[0]?.payload;
-    if (channel && channel.items && channel.items[1]?.data) {
-      console.log('进入发现页...');
-      if (channel.items[1].data.itemid === "hot_search_push") {
-        index = 2;
-      }
-
-      // 1.1、下标是1的为热搜模块
-      channel.items[index].data.group = removeHotSearchAds(channel.items[index].data.group);
-
-      // 1.2、下标为2的是轮播图模块
-      console.log('移除轮播模块💕💕');
-      channel.items[index + 1] = {};
-
-      // 1.3、items[i].category = "feed" 是热门微博的部分
-      channel.items = removeCategoryFeedAds(channel.items);
-
-      return JSON.stringify(resp_data);
+    const payload = resp_data.channelInfo?.channels?.[0]?.payload;
+    console.log('进入发现页...');
+    if (payload.items[1].data.itemid === "hot_search_push") {
+      index = 2;
     }
+
+    // 1.1、下标是1的为热搜模块
+    payload.items[index].data.group = removeHotSearchAds(payload.items[index].data.group);
+
+    // 1.2、下标为2的是轮播图模块
+    console.log('移除轮播模块💕💕');
+    payload.items[index + 1] = {};
+
+    // 1.3、下标为3的是热议模块
+    console.log('移除finder_channel模块💕💕');
+    if (payload.items[index + 2].data.more_pic.includes('ads')) {
+      payload.items[index + 2].data.more_pic = getRandomWeiboPic();
+    }
+    payload.items[index + 2].data.group = removeFinderChannelAds(payload.items[index + 2].data.group);
+
+    // 1.4、items[i].category = "feed" 是热门微博的部分
+    payload.items = removeCategoryFeedAds(payload.items);
+    return JSON.stringify(resp_data);
   }
 
   // 2、发现页面刷新/再次点击发现按钮
@@ -61,7 +80,13 @@ function modifyMain(url, data) {
     console.log('移除轮播图模块🤣🤣');
     resp_data.items[index + 1] = {};
 
-    // 2.3、items[i].category = "feed" 是热门微博的部分
+    // 2.3、下标为3的是热议模块
+    if (resp_data.items[index + 2].data.more_pic?.includes('ads')) {
+      resp_data.items[index + 2].data.more_pic = getRandomWeiboPic();
+    }
+    resp_data.items[index + 2].data.group = removeFinderChannelAds(resp_data.items[index + 2].data.group);
+
+    // 2.4、items[i].category = "feed" 是热门微博的部分
     resp_data.items = removeCategoryFeedAds(resp_data.items);
 
     return JSON.stringify(resp_data);
@@ -98,8 +123,22 @@ function modifyMain(url, data) {
 // 移除“微博热搜”的广告
 function removeHotSearchAds(groups) {
   console.log('移除发现页热搜广告开始💕');
-  const newGroups = groups.filter(group => !(group && group.item_log && group.item_log.adid));
+  const newGroups = groups.filter(group => !(group.item_log && group.item_log.adid));
   console.log('移除发现页热搜广告结束💕💕');
+  return newGroups;
+}
+
+// 移除“微博热搜”下面的“热聊，本地等”的广告
+function removeFinderChannelAds(groups) {
+  console.log('移除发现页finder_channel广告开始💕');
+  const newGroups = [];
+  for (const group of groups) {
+    if (group.hasOwnProperty('pic') && typeof group.pic === 'string' && group.pic.includes('ads')) {
+      group.pic = getRandomWeiboPic();
+    }
+    newGroups.push(group);
+  }
+  console.log('移除发现页finder_channel广告结束💕💕');
   return newGroups;
 }
 
@@ -109,4 +148,9 @@ function removeCategoryFeedAds(items) {
   const newItems = items.filter(item => item.category !== "feed" || (item.data && item.data.mblogtypename !== "广告"));
   console.log('移除发现页热门微博广告结束💕💕');
   return newItems;
+}
+
+function getRandomWeiboPic() {
+  const randomIndex = Math.floor(Math.random() * weiboPic.length);
+  return weiboPic[randomIndex];
 }
