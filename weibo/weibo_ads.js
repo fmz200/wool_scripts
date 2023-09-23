748c77aca83772312704bb631923f7231871eaf6
king <zfm6633@gmail.com>
2023-09-23 21:51:13 +0800

    update weibo_ads.js

--- weibo/weibo_ads.js
+++ weibo/weibo_ads.js
@@ -171,7 +171,7 @@ function removeFinderChannelAds(groups) {
   for (const group of groups) {
     if (group.hasOwnProperty('pic') && group.pic.includes('ads')) {
       // https://h5.sinaimg.cn/upload/100/972/2022/06/13/timeline_location_default.png
-      group.pic = titleSubPicMap[group.title_sub] || "https://tvax1.sinaimg.cn/crop.0.0.800.800.180/005T3xyoly8h36u4j3h1mj60m80m874z02.jpg";
+      group.pic = titleSubPicMap[group.title_sub] || "https://simg.s.weibo.com/20200915_huodong.png";
     }
     newGroups.push(group);
   }
