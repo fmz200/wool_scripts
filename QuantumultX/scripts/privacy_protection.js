99e474d77abbab451e1a6b3112c3e602491bc7f9
king <zfm6633@gmail.com>
2023-10-10 22:37:16 +0800

    update something

--- QuantumultX/scripts/privacy_protection.js
+++ QuantumultX/scripts/privacy_protection.js
@@ -1,19 +1,7 @@
 /**
  * author:fmz200
- * date:2023-10-09 22:55:13
+ * date:2023-10-09 22:55:00
+ * function:不分APP收集用户信息，将上传的内容换成空值
  */
 
-const url_001 = "/api-a.soulapp.cn/loveBell/uploadPosition";
-const url_002 = "/insight.soulapp.cn/mobile/insight/log/upload";
-
-let url = $request.url;
-let body = $request.body;
-
-if (url.includes(url_001) || url.includes(url_002)) {
-  console.log('匹配到URL✅');
-  $done({body: {}});
-} else {
-  console.log('没有匹配到URL⁉️');
-  $done({body: JSON.stringify(body)});
-}
-
+$done({body: {}});
