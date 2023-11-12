8a50637a83f8155edaf369047ffc68dbde149dc8
king <zfm6633@gmail.com>
2023-11-12 21:53:17 +0800

    update something

--- Scripts/ccbLife/ccbLifeCookie.js
+++ Scripts/ccbLife/ccbLifeCookie.js
@@ -2,9 +2,9 @@ const cookieName = '建行生活签到';
 const signUrlKey = 'photonmang_signurl_jhsh';
 const signHeaderKey = 'photonmang_signheader_jhsh';
 const signBodyKey = 'photonmang_signbody_jhsh';
-const photonmang = init();
+const globalTools = init();
 
-if ($request && $request.method != 'OPTIONS') {
+if ($request && $request.method !== 'OPTIONS') {
   const signUrlVal = $request.url;
   const signHeaderVal = JSON.stringify($request.headers);
   const signBodyVal = JSON.stringify($request.body);
@@ -13,43 +13,43 @@ if ($request && $request.method != 'OPTIONS') {
   console.log(`签到header：${signHeaderVal}`);
   console.log(`签到Body：${signBodyVal}`);
 
-  if (signUrlVal) photonmang.setdata(signUrlVal, signUrlKey);
-  if (signHeaderVal) photonmang.setdata(signHeaderVal, signHeaderKey);
-  if (signBodyVal) photonmang.setdata(signBodyVal, signBodyKey);
+  if (signUrlVal) globalTools.setData(signUrlVal, signUrlKey);
+  if (signHeaderVal) globalTools.setData(signHeaderVal, signHeaderKey);
+  if (signBodyVal) globalTools.setData(signBodyVal, signBodyKey);
 
-  photonmang.msg(cookieName, `获取Cookie: 成功`, ``);
+  globalTools.msg(cookieName, `获取Cookie: 成功`, ``);
 }
 
 function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true;
+  let isSurge = () => {
+    return this.$httpClient !== undefined;
   }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true;
+  let isQuanX = () => {
+    return this.$task !== undefined;
   }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
+  let getData = (key) => {
+    if (isSurge()) return $persistentStore.read(key);
+    if (isQuanX()) return $prefs.valueForKey(key);
   }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  let setData = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val);
+    if (isQuanX()) return $prefs.setValueForKey(key, val);
   }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
+  let msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body);
+    if (isQuanX()) $notify(title, subtitle, body);
   }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
+  let log = (message) => console.log(message);
+  let get = (url, cb) => {
     if (isSurge()) {
-      $httpClient.get(url, cb)
+      $httpClient.get(url, cb);
     }
     if (isQuanX()) {
       url.method = 'GET'
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
   }
-  post = (url, cb) => {
+  let post = (url, cb) => {
     if (isSurge()) {
       $httpClient.post(url, cb)
     }
@@ -58,10 +58,10 @@ function init() {
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
   }
-  done = (value = {}) => {
+  let done = (value = {}) => {
     $done(value)
   }
-  return {isSurge, isQuanX, msg, log, getdata, setdata, get, post, done}
+  return {isSurge, isQuanX, msg, log, getData, setData, get, post, done}
 }
 
-photonmang.done()
+globalTools.done();
