444c56e95e8bf3e05fbcabb27379c92d085fd179
king <zfm6633@gmail.com>
2023-12-16 13:33:55 +0800

    update something

--- Scripts/shunfeng_json.js
+++
@@ -1,7 +0,0 @@
-if ($request.url.indexOf("app/ad/queryInfoFlow") != -1) {
-  var new_body = JSON.parse($response.body);
-  new_body.obj = Object.values(new_body.obj).filter((item) => item.adverId == 2833);
-  $done({
-    body: JSON.stringify(new_body),
-  });
-}
