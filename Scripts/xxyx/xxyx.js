5e23a1df13b270d50ba180bef8ea5754bd60e656
奶思(⸝⸝•‧̫•⸝⸝) <57740735+fmz200@users.noreply.github.com>
2024-05-16 08:17:51 +0800

    更新 xxyx.js

--- Scripts/xxyx/xxyx.js
+++ Scripts/xxyx/xxyx.js
@@ -1,7 +1,7 @@
 /**
  * @author fmz200
  * @function 晓晓优选 测试
- * @date 2024-02-22 20:20:13
+ * @date 2024-05-16 08:16:13
  */
 
 let requestUrl = $request.url;
@@ -10,6 +10,7 @@ let responseBody = $response.body;
 let obj = JSON.parse(responseBody);
 
 // 能量解锁，可以直接兑换好礼
+// 官方加了验证，已不可用
 // ^https:\/\/xxyx-client-api\.xiaoxiaoyouxuan\.com\/client\/energy\/mall\/getUserEnergy url script-response-body xxyx.js
 if (requestUrl.includes("/client/energy/mall/getUserEnergy")) {
   obj.data.energy = 10086;
