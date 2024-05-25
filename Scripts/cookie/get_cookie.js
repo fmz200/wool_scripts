212c919be38b73fd4d6a4d55653ed35e4adb43e7
king <zfm6633@gmail.com>
2024-05-25 23:15:41 +0800

    update Loon config

--- Scripts/cookie/get_cookie.js
+++ Scripts/cookie/get_cookie.js
@@ -207,13 +207,14 @@ try {
     if (token && rsp_data) {
       let mobile = rsp_data.mobile;
       let username = rsp_data.nick;
+      let avatar = rsp_data.avatar;
       console.log(`获取到uid：${mobile}，username：${username}`);
 
       let cache = $.read("#fmz200_xxyx_token") || "[]";
       console.log("读取缓存数据：" + cache);
 
       let json_data = JSON.parse(cache);
-      updateOrAddObject(json_data, "mobile", mobile, "username", username, "token", token);
+      updateOrAddObject(json_data, "mobile", mobile, "username", username, "token", token, "avatar", avatar);
       const cacheValue = JSON.stringify(json_data, null, "\t");
       
       $.write(cacheValue, '#fmz200_xxyx_token');
