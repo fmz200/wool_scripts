5714fd6558b29117f34bc1137ddab3054ba39687
king <zfm6633@gmail.com>
2024-08-11 12:06:42 +0800

    update xianyu

--- Scripts/xianyu/xianyu_ads.js
+++ Scripts/xianyu/xianyu_ads.js
@@ -1,15 +1,15 @@
-// 2024-08-10 22:33:37
+/**
+ * 闲鱼 去广告+净化
+ * 2024-08-10 22:33:37
+ */
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/gw/mtop.taobao.idlehome.home.nextfresh")) {
-  if (obj.data && obj.data.sections) {
+  if (obj.data?.sections) {
     obj.data.sections = obj.data.sections.filter(section => {
-      if (section.template && (section.template.name === "fish_home_advertise_card_d4" || section.data.bizType === "AD" || section.data.bizType === "homepage")) {
-        return false; // 删除该数组项
-      }
-      return true; // 保留该数组项
+      return !(section.data && (section.data.bizType === "AD" || section.data.bizType === "homepage"));
     });
   }
 }
