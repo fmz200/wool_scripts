a73be4481718b0a96ecee5a21d24bd922eacd380
king <zfm6633@gmail.com>
2025-04-16 21:23:25 +0800

    update something

--- Scripts/555Ad.js
+++ Scripts/555Ad.js
@@ -1,1 +1,11 @@
-let obj=JSON.parse($response.body);obj.data=obj.data.filter(t=>"advert_self"!==t.layout),obj.data.forEach(t=>{t.list=t.list.filter(t=>3!==t.type)}),$done({body:JSON.stringify(obj)});
+let obj = JSON.parse($response.body);
+
+// 过滤掉 layout 为 advert_self 的 data 条目
+obj.data = obj.data.filter(item => item.layout !== "advert_self");
+
+// 对每个 data 条目中的 list 进行二次过滤 (移除 type=3 的元素)
+obj.data.forEach(item => {
+  item.list = item.list.filter(ad => ad.type !== 3);
+});
+
+$done({body: JSON.stringify(obj)});
