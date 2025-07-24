dcfc57d364438ad0a88a3a1fb05d3036cf299078
king <zfm6633@gmail.com>
2025-07-24 22:55:51 +0800

    update something

--- Scripts/weibo/weibo_ads.js
+++ Scripts/weibo/weibo_ads.js
@@ -1,7 +1,7 @@
 /**
  * @author fmz200
  * @function 微博去广告
- * @date 2025-07-24 22:30:00
+ * @date 2025-07-09 19:00:00
  */
 
 let url = $request.url;
@@ -155,7 +155,7 @@ try {
     console.log('处理评论区广告结束💕💕');
   }
   if (url.includes("/statuses/container_detail_comment?")) {
-    resp_data.items = resp_data.items.filter(item => item.category === "detail");
+    resp_data.items = resp_data.items.filter(item => item.type !== "trend" && !item.commentAdType);
     console.log('处理评论区广告结束💕💕');
   }
   
